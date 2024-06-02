f3938ba350421b60a543ecbaa68231c18ac0649c
Divyansh-1975 <divyansh.1975.sharma@gmail.com>
2024-06-02 12:24:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,16 +22,33 @@ class Paper {
 
   init(paper) {
     document.addEventListener('mousemove', (e) => {
-      if (!this.touchActive && !this.rotating && this.holdingPaper) {
-        this.velX = e.clientX - this.prevTouchX;
-        this.velY = e.clientY - this.prevTouchY;
-        this.currentPaperX += this.velX;
-        this.currentPaperY += this.velY;
-        this.prevTouchX = e.clientX;
-        this.prevTouchY = e.clientY;
-        this.updatePaperTransform(paper);
+      if(!this.rotating) {
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY;
       }
-    });
+      const dirX = e.clientX - this.mouseTouchX;
+      const dirY = e.clientY - this.mouseTouchY;
+      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = 180 * angle / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+      if(this.rotating) {
+        this.rotation = degrees;
+      }
+      if(this.holdingPaper) {
+        if(!this.rotating) {
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
+        }
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+    })
 
     paper.addEventListener('mousedown', (e) => {
       if(this.holdingPaper) return;
@@ -43,8 +60,6 @@ class Paper {
         this.mouseTouchY = this.mouseY;
         this.prevMouseX = this.mouseX;
         this.prevMouseY = this.mouseY;
-        this.touchX = this.prevTouchX = e.clientX;
-        this.touchY = this.prevTouchY = e.clientY;
       }
       if(e.button === 2) {
         this.rotating = true;
@@ -121,3 +136,25 @@ papers.forEach(paper => {
   const p = new Paper();
   p.init(paper);
 });
+
+/*
+// Audio Player
+document.addEventListener("DOMContentLoaded", function() {
+  const audio = document.getElementById("audio");
+  const playButton = document.getElementById("playButton");
+  const pauseButton = document.getElementById("pauseButton");
+  const stopButton = document.getElementById("stopButton");
+
+  playButton.addEventListener("click", function() {
+      audio.play();
+  });
+
+  pauseButton.addEventListener("click", function() {
+      audio.pause();
+  });
+
+  stopButton.addEventListener("click", function() {
+      audio.pause();
+      audio.currentTime = 0;
+  });
+}); */
